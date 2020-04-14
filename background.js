899bfeb646a5f96a0b61e2c67d3e59cf57c7c8b5
samhippo <63636707+samhippo@users.noreply.github.com>
2020-04-14 07:15:10 -0700

    Add files via upload

--- background.js
+++ background.js
@@ -1,19 +1,14 @@
 "use strict";
 
-var SAVED_SEARCH_TERM = ''; //the only data saved in the background script is the search term
-/**
- * Called from find.js (popup page)
- */
+var SAVED_SEARCH_TERM = '';
+
 async function notify(request, sender, sendResponse) {
     if (request.name == "LOAD") {
-        return Promise.resolve(SAVED_SEARCH_TERM); //retrieves temporary data when the popup is opened
-
+        return Promise.resolve(SAVED_SEARCH_TERM); //returns saved data to popup page
     } else if (request.name == "SAVE_INPUT_TEXT") {
         SAVED_SEARCH_TERM = request.value; //search term is saved as the user types
-
     } else if (request.name == "SAVE_CLEAR") {
         SAVED_SEARCH_TERM = '';
-
     }
 }
 
@@ -21,18 +16,16 @@ function reportError(error) {
     console.error(`report error background.js: ${error}`);
 }
 
-/**
- * Called whenever the active tab changes
- * this is used to stop any unecessary timers to free up resources
- * it also forces the popup page to close, normally this happens anyways but not with hotkeys
- */
 function tabChanged(activeInfo) {
     if (activeInfo.previousTabId) {
+        //if previous tab exists then try to stop the timer
         browser.tabs.sendMessage(activeInfo.previousTabId, { name: "STOP_TIMER" }).catch(reportError);
     }
     if (activeInfo.tabId) {
+        //try to activate timer if it exists
         browser.tabs.sendMessage(activeInfo.tabId, { name: "START_TIMER" }).catch(reportError);
     }
+    //close popup if it's open
     browser.runtime.sendMessage({ "name": "CLOSE" }).catch(reportError);
 }
 
@@ -40,10 +33,7 @@ function tabChanged(activeInfo) {
  * Not needed
  */
 async function tabUpdated(tabId, changeInfo, tabInfo) {
-
-    if (changeInfo.status == 'complete') {
-
-    }
+    if (changeInfo.status == 'complete') {}
 }
 
 browser.runtime.onMessage.addListener(notify);
